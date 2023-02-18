2b2c0dbd44102c2bc74f8dc8de529f51b826e1b3
Dorian Andrés <dcortesg441@ulacit.ed.cr>
2023-02-18 01:35:54 -0600

    improving start script

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,4 +1,4 @@
-start cmd /k "yarn build:lib"
-start cmd /k "yarn start:server"
-start cmd /k "yarn start:client"
+start cmd /c "yarn build:lib:watch"
+start cmd /c "yarn start:server"
+start cmd /c "yarn start:client"
 cls
